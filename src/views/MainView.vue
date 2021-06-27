78f69e46d19690b9e2c464a2ba654315b9ce080c
RyoMa99 <hatanakaryoma.130@gmail.com>
2021-06-27 05:10:44 +0000

    init

---
+++ src/views/MainView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>init</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({})
+export default class extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
